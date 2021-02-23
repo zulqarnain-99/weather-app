a687f6ef7d868ca170dd3fa3e98d80e6b9dc6788
zulqarnain-99 <zulqi99@gmail.com>
2021-02-23 12:06:21 +0500

    building JSON Http endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const hbs = require("hbs")
+const geocode = require("./utils/forecast")
 
 const app = express()
 
@@ -46,11 +47,29 @@ app.get("/weather", (req, response) => {
       error: "you must provide an address!",
     })
   }
-  response.send({
-    forecast: "It is snowing",
-    location: "Philendelphia",
-    address: req.query.address,
+
+  geocode(req.query.address, (error, {latitude, longitude, location}){
+    if(error){
+      return response.send({error})
+    }
+
+    forecast(latitude, (error, forecastData) => {
+      if(error){
+        return response.send({error})
+      }
+      response.send({
+        forecast: forecastData,
+        location,
+        address: res.query.address
+      })
+
+    })
   })
+  // response.send({
+  //   forecast: "It is snowing",
+  //   location: "Philendelphia",
+  //   address: req.query.address,
+  // })
 })
 
 app.get("*", (req, response) => {
